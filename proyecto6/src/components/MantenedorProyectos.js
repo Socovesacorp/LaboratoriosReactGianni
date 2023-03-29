3913819dab990d2b0a4f508a390fff588b935ef0
Gianni Palmieri <39269011+gpalmierisocovesa@users.noreply.github.com>
2023-03-29 15:39:24 -0300

    Llevar Todo a la Nube

---
+++ proyecto6/src/components/MantenedorProyectos.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function MantenedorProyectos() {
+  return (
+    <div>
+      <h1>hola cómo están?????</h1>
+    </div>
+  );
+}
+
+export default MantenedorProyectos;
